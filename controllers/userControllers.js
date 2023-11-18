22bcb5cbe4108dc77ee77638575cff5b52da5937
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-11-18 17:24:40 +0530

    basic commit

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,1 +1,9 @@
 const user = require('../models/userModel');
+
+const uploadFile = (req, res) => {
+    const {avatar} = req.body
+    const newUser = new user({
+        avatar
+    })
+    newUser.save() 
+}
